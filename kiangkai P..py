683e4906d5ed91358654fca6cebd806b5931d0e3
Kiangkai62 <52350403+Kiangkai62@users.noreply.github.com>
2019-08-03 22:10:23 +0700

    Add files via upload

---
+++ kiangkai P..py
@@ -0,0 +1,8 @@
+b = 40
+d = 45
+f = 50
+h = 60
+print("Foundation English" + str(b))
+print("Geuneral Business" + str(d))
+print("Introduction to Computer Systems" + str(f))
+print("Computer Programming" + str(h))
